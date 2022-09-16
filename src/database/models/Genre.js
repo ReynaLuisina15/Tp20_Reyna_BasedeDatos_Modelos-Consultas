7051d4b9bf154ce1eaa97d455e45174f96973904
reynaluisina15 <reyna.luisina@gmail.com>
2022-09-15 22:37:16 -0300

    primer commit

---
+++ src/database/models/Genre.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, dataTypes) => {
+
+    const alias = "Genre";
+
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        name: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        ranking: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            defaultValue: "1",
+        },
+        active: {
+            type: dataTypes.INTEGER(1),
+            allowNull: false,
+            defaultValue: "1",
+        },
+    };
+
+    const config = {
+        tableName : "genres",
+        timestamps : true,
+        underscored : true
+    }
+
+
+    const Genre = sequelize.define(alias, cols, config);
+
+    return Genre
+}
