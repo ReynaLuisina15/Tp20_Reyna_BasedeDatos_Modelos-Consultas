6b7672e0fe0279cbe14861671ba86791fcc7b70b
reynaluisina15 <reyna.luisina@gmail.com>
2022-09-27 21:04:41 -0300

    guardando datos

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,11 +1,50 @@
-const db = require('../database/models')
+const { render } = require("ejs");
+const db = require("../database/models");
 
 module.exports = {
-    list: (req, res) => {
-        db.Movie.findAll()
-            .then((movies) => {
-                return res.send(movies)
-            })
-            .catch((error) => console.log(error))
-    }
+  list: (req, res) => {
+    db.Movie.findAll()
+      .then((movies) => {
+        /* TODA RESPUESTA TIENE QUE ESTAR DENTRO DEL THEN CUANDO HAY CONSULTA A LA BASE DE DATOS */
+        return res.render("moviesList", {
+          movies
+        })
+      })
+      .catch(error => console.log(error))
+  },
+  detail: (req, res) => {
+    db.Movie.findByPk(req.params.id)
+      .then(movie => {
+        return res.render("moviesDetail", {
+          movie
+        })
+      })
+      .catch(error => console.log(error))
+  },
+  new : (req,res) => {
+    db.Movie.findAll({
+        order : [
+            ['release_date', 'DESC'],
+        ]
+    })
+    .then(movies => {
+        return render.render('newestMovies', {
+            movies
+        })
+    })
+    .catch(error => console.log(error))
+  },
+  recomended : (req,res) => {
+    db.Movie.findAll({
+        order : [
+            ['rating', 'DESC']
+        ],
+        limit : 5
+    })
+    .then(movies => {
+        return render.render('recommendedMovies', {
+            movies
+        })
+    })
+  }
 }
